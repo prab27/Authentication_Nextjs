dd1ce8f38ee6170bae785965b9e76eca8611788c
prab27 <ayushsin72@gmail.com>
2024-04-15 03:25:09 +0530

    update

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,19 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+import { getDataFromToken } from "@/helpers/detDataFromToken";
+
+
+connect()
+
+export async function POST(request: NextRequest){
+    //extract data  from token 
+    const userId = await getDataFromToken(request)
+    const user = await User.findOne({_id: userId}).select("-password")
+    //check if there is no user 
+    return NextResponse.json({
+        message:"UserFound",
+        data:user
+    })
+}
