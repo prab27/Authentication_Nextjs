28340991d252bbb6473a6d37581883775e4a2f51
prab27 <ayushsin72@gmail.com>
2024-04-19 15:15:57 +0530

    update

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,10 +1,73 @@
 'use client'
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default function VerifyEmailPage() {
-    useState
+     //const router = useRouter()
+
+
+
+    const[token,settoken] = useState("")
+    const[verified,setVerified] = useState(false)
+    const [error,setError] =useState(false)
+
+     const verifyUserEmail = async () => {
+        try {
+            await axios.post("/api/users/verifyemail",{token})
+            setVerified(true)
+            setError(false)
+        } catch (error: any) {
+            setError(true)
+            console.log(error.response.data);
+            
+        }
+
+     }
+
+     useEffect(()=>{
+        setError(false)
+       const urlToken = window.location.search.split('=')[1]
+       settoken(urlToken || "")
+      
+       //const {query} = router;
+       //const urlTokenTwo =query.token
+
+
+
+     },[])
+
+     useEffect(() => {
+        setError(false)
+        if(token.length > 0){
+            verifyUserEmail()
+
+        }
+     },[token])
+
+
   return (
-    <div>page</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+
+    <h1 className="text-4xl">Verify Email</h1>
+    <h2 className="p-2 bg-orange-500 text-black">{token ? `${token}` : "no token"}</h2>
+
+    {verified && (
+        <div>
+            <h2 className="text-2xl">Email Verified</h2>
+            <Link href="/login">
+                Login
+            </Link>
+        </div>
+    )}
+    {error && (
+        <div>
+            <h2 className="text-2xl bg-red-500 text-black">Error</h2>
+            
+        </div>
+    )}
+</div>
   )
 }
 
